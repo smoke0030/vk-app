b3c9edbe5fdd62642f0cbab8024e40d41fbe59d5
Sergey Vidishev <ft.priest@gmail.com>
2024-07-29 13:22:38 +0300

    Initial Commit

---
+++ vk-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-app
+//
+//  Created by Сергей on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
