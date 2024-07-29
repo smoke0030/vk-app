2b23dcfe1b1d49b11f3da9a0494a3e843841266f
Sergey Vidishev <ft.priest@gmail.com>
2024-07-29 16:13:47 +0300

    интегрирован firebase

---
+++ vk-app/TabBar/Feed/APIManager.swift
@@ -0,0 +1,16 @@
+//
+//  APIManager.swift
+//  vk-app
+//
+//  Created by Сергей on 29.07.2024.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import FirebaseStorage
+import FirebaseDatabase
+
+class APIManager {
+    
+}
---
+++ vk-app/TabBar/Feed/FeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedViewController.swift
+//  vk-app
+//
+//  Created by Сергей on 29.07.2024.
+//
+
+import UIKit
+
+
+final class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
